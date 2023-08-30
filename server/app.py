d0485ec8605e7b000c3cf4e95a34ed09a77dd750
Denzel <jdenzelmab@gmail.com>
2023-08-30 18:32:22 -0400

    Completed flask application structure lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+@app.route('/') #Routing: the association of URLS
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
